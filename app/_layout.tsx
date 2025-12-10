829f7ce66354524790d2303806260aed43ae956b
Agustín Cassani <agustinscassani@gmail.com>
2025-12-10 07:06:13 -0300

    Beneficiary mobile app cleaned up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,5 @@
-import React from 'react';
-import { Stack } from 'expo-router';
-import { AuthProvider } from '../contexts/AuthContext';
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="login" />
-      </Stack>
-    </AuthProvider>
-  );
+  return <Stack />;
 }
